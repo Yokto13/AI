3e4454f71a6c2d2029d43a965ee9ac2fcba38242
Dominik Farhan <nikousf@seznam.cz>
2023-10-02 10:05:43 +0200

    working implementation of classification tree

--- implementations/trees/classification_tree.py
+++ implementations/trees/classification_tree.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Generator
 
 import numpy as np
-from sklearn.base import BaseEstimator, RegressorMixin
 
 
 @dataclass
@@ -23,16 +22,22 @@ class Node:
             return self.right.predict(x)
 
 
-class RegressionTree(BaseEstimator, RegressorMixin):
-    def __init__(self, max_depth: int):
+class ClassificationTree:
+    def __init__(self, n_of_classes: int, max_depth: int,
+                 ftr_generator: Generator[int, int, None] = range):
+        self.n_of_classes = n_of_classes
         self.max_depth = max_depth
         self.root: Node | None = None
+        self.ftr_generator = ftr_generator
+
+    def target_to_pd(self, y):
+        pds = np.zeros((y.size, self.n_of_classes))
+        pds[np.arange(y.size), y] = 1
+        return np.average(pds, axis=0)
 
     def build_tree(self, X: np.array, y: np.array, depth) -> Optional[Node]:
-        if len(X) == 0:  # due to numerical instability sometimes split produces empty part
-            return None
         if depth == self.max_depth or len(X) == 1:
-            target = y.mean()
+            target = self.target_to_pd(y)
             return Node(
                 ftr_index=None,
                 ftr_val=None,
@@ -43,7 +48,7 @@ class RegressionTree(BaseEstimator, RegressorMixin):
             )
         ftr_index, ftr_val, X, y = self.find_best_split(X, y)
         if ftr_index is None:
-            target = y.mean()
+            target = self.target_to_pd(y)
             return Node(
                 ftr_index=None,
                 ftr_val=None,
@@ -63,27 +68,25 @@ class RegressionTree(BaseEstimator, RegressorMixin):
         )
 
     def find_best_split(self, X: np.array, y: np.array) -> tuple[int, float, np.array, np.array]:
-        var = lambda p2, p1, cnt: (p2 / cnt - (p1 / cnt) ** 2) * (cnt / len(X))
-        Split = namedtuple("Split", ['var', 'ftr_val', 'ftr_idx'])
-        best_result = Split(np.inf, None, None)
-        y_squared = np.power(y, 2)
-        no_split_var = var(np.sum(y_squared), np.sum(y), len(y))
-        for ftr_idx in range(X.shape[-1]):
+        def entropy(y):
+            unique, counts = np.unique(y, return_counts=True)
+            tot = sum(counts)
+            return - sum((x / tot * np.log(x) for x in counts))
+
+        Split = namedtuple("Split", ['score', 'ftr_val', 'ftr_idx'])
+        no_split_score = entropy(y)
+        best_result = Split(no_split_score, None, None)
+        for ftr_idx in self.ftr_generator(X.shape[-1]):
             sorted_inds = np.argsort(X[:, ftr_idx])
-            X, y, y_squared = X[sorted_inds], y[sorted_inds], y_squared[sorted_inds]
-            left_squared, right_squared = 0, np.sum(y_squared)
-            left_sum, right_sum = 0, np.sum(y)
-            ftr_vals = sorted([f for f in set(ff for ff in X[:, ftr_idx])])
-            for left_size, ftr_val in enumerate(X[:len(X) - 1, ftr_idx]):
-                left_squared += y_squared[left_size]
-                right_squared -= y_squared[left_size]
-                left_sum += y[left_size]
-                right_sum -= y[left_size]
-                left_var = var(left_squared, left_sum, left_size + 1)
-                right_var = var(right_squared, right_sum, len(y) - left_size - 1)
-                tot_var = left_var + right_var
-                if tot_var < best_result.var and tot_var < no_split_var and ftr_vals.index(ftr_val) < len(ftr_vals) - 1:
-                    best_result = Split(tot_var, (ftr_val + ftr_vals[ftr_vals.index(ftr_val) + 1]) / 2, ftr_idx)
+            for i in range(len(sorted_inds) - 1):
+                if X[i, ftr_idx] == X[i + 1, ftr_idx]:
+                    continue
+                y_left, y_right = y[sorted_inds[:i + 1]], y[sorted_inds[i + 1:]]
+                score_left, score_right = entropy(y_left), entropy(y_right)
+                split_score = score_left + score_right
+                if split_score < best_result.score:
+                    val = (X[i, ftr_idx] + X[i + 1, ftr_idx]) / 2
+                    best_result = Split(split_score, val, ftr_idx)
         if best_result.ftr_idx is not None:
             sorted_inds = np.argsort(X[:, best_result.ftr_idx])
             X, y = X[sorted_inds], y[sorted_inds]
@@ -106,21 +109,25 @@ class RegressionTree(BaseEstimator, RegressorMixin):
     def predict(self, X):
         return [self.root.predict(x) for x in X]
 
+    def hard_predict(self, X):
+        return [np.argmax(x) for x in self.predict(X)]
+
 
 if __name__ == "__main__":
-    from sklearn.datasets import load_diabetes
+    from sklearn.datasets import load_iris
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import mean_squared_error
-    from sklearn.tree import DecisionTreeRegressor
+    from sklearn.tree import DecisionTreeClassifier
 
-    data = load_diabetes()
+    data = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(data['data'], data['target'], test_size=0.3, shuffle=True,
                                                         random_state=10)
     print(len(X_train))
-    rt = RegressionTree(5)
+    rt = ClassificationTree(y_train.max() + 1, 5)
     rt.fit(X_train, y_train)
-    y_pred = rt.predict(X_test)
+    pres = rt.predict(X_test)
+    y_pred = np.argmax(rt.predict(X_test), axis=1)
     print(mean_squared_error(y_test, y_pred))
-    dtr = DecisionTreeRegressor(max_depth=5).fit(X_train, y_train)
+    dtr = DecisionTreeClassifier(max_depth=5).fit(X_train, y_train)
     y_pred = dtr.predict(X_test)
     print(mean_squared_error(y_test, y_pred))
